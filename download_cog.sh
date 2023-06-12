9c617bd15d22fa08b184b9004443f5def17290da
Biel Stela <biel.stela@gmail.com>
2023-06-12 16:49:05 +0200

    some updates

---
+++ download_cog.sh
@@ -0,0 +1,3 @@
+wget -P data/ https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/31/S/DD/2023/4/S2B_31SDD_20230407_0_L2A/B02.tif;
+wget -P data/ https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/31/S/DD/2023/4/S2B_31SDD_20230407_0_L2A/B03.tif;
+wget -P data/ https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/31/S/DD/2023/4/S2B_31SDD_20230407_0_L2A/B04.tif;
